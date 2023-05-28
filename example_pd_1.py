942a8b2b71769f33195cd3844083d8dc78f393a1
Mohammad Farook <mdfk101@gmail.com>
2023-05-27 22:55:41 -0700

    Update example_pd_1.py

--- example_pd_1.py
+++ example_pd_1.py
@@ -10,6 +10,6 @@ OPENAI_API_KEY = "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"
 llm = OpenAI(api_token=OPENAI_API_KEY)
 pandas_ai = PandasAI(llm)
 
-#print(pandas_ai.run(df, prompt='Which is the most expensive Brand'))
+print(pandas_ai.run(df, prompt='Which is the most expensive Brand'))
 print(pandas_ai.run(df, prompt='Which brand has the best quality based on customer ratings battery life only'))
-#print(pandas_ai.run(df, prompt='List the brands in terms of quality, where best quality is listed first'))
+print(pandas_ai.run(df, prompt='List the brands in terms of quality, where best quality is listed first'))
